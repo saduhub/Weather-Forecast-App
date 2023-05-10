2559dded60b4ce79ea55759be6788736f118e6e2
Jorge Duarte <sadujorge@gmail.com>
2023-05-09 18:55:21 -0700

    finished basic html, css and js API retireval of data

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,6 @@
-require('dotenv').config();
-const $ = require('jquery');
-const apiKey = process.env.API_KEY;
-console.log(apiKey);
+const apiKey = '';
+
+
 
 // url will be modified depending on the query made
 let queryInput = $('.query-item');
@@ -11,9 +10,12 @@ let cardContainer = $('#results');
 submitButton.on('click', function(event) {
     event.preventDefault();
     let searchTerm = queryInput.val();
-    let weatherApi = `https://www.loc.gov/books/?fo=json&q=${searchTerm}&sp=1&c=5`
-    
-    fetch(weatherApi)
+    let weatherApiCoordinates = `http://api.openweathermap.org/geo/1.0/direct?q=${searchTerm}&limit=5&appid=${apiKey}`
+    let lat;
+    let lon;
+
+// get lat and lon
+    fetch(weatherApiCoordinates)
         .then(function(response) {
             if (response.ok) {
                 return response.json();
@@ -22,31 +24,94 @@ submitButton.on('click', function(event) {
             }
         })
         .then(function(data) {
-            let books = data.results;
-            console.log(books);
-
-            cardContainer.empty(); // Clear any previous cards from the container
-
-            for (let i = 0; i < books.length; i++) {
-                let book = books[i];
-                let title = book.title;
-                let description = book.description;
-                let url = book.url;
-
-                let card = $('<div>').addClass('card w-100');
-                let innerCard = $('<div>').addClass('card-body');
-                let cardTitle = $('<h5>').addClass('card-title').text(title);
-                let cardText = $('<p>').addClass('card-text').text(description);
-                let cardLink = $('<a>').addClass('btn btn-primary').attr('href', url).text('Visit');
-
-                innerCard.append(cardTitle, cardText, cardLink);
-                card.append(innerCard);
-                cardContainer.append(card); // Append each card to the container
-            }
-
+            lat = data[0].lat;
+            lon = data[0].lon;
+            console.log(lat, lon);
         })
+        .then (function() {
+            let weatherApiToday = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`
+            fetch(weatherApiToday)
+                .then(function(response) {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        alert('Error: ' + response.statusText);
+                    }
+                })
+                .then(function(data) {
+                    let city = data.name;
+
+                    console.log(city);
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    alert(error.message);
+                });
+                })
+        .then (function() {
+            let weatherApiForecast = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
+            fetch(weatherApiForecast)
+                .then(function(response) {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        alert('Error: ' + response.statusText);
+                    }
+                })
+                .then(function(data) {
+                    let dataList = data.list;
+                    for (let i=0; i < dataList.length; i++) {
+                        
+                        let mayTenTwoPm = 1683752400;
+                        let timestamp = dataList[i].dt;
+
+                        if ((timestamp-mayTenTwoPm)%86400 === 0) {
+                            let formatDate = dayjs.unix(timestamp).format('MM/DD/YYYY h:mm A');
+                            let kelvin = dataList[i].main.temp;
+                            let celsius = Math.floor(dataList[i].main.temp) - 273;
+                            let fahrenheit = Math.floor((dataList[i].main.temp - 273.15)* 1.8) + 32;
+                            let humidity = dataList[i].main.humidity;
+                            let wind = Math.floor(dataList[i].wind.speed * 2.23694);
+                            let icon = dataList[i].weather[0].icon;
+                            console.log(formatDate);
+                            console.log(`${celsius} C`);
+                            console.log(`${fahrenheit} F`);
+                            console.log(`${kelvin} K`);
+                            console.log(`${humidity}%`);
+                            console.log(`${wind} mph`);
+                            console.log(`${icon} Icon`);
+
+                            // let weatherApiIcon = `https://openweathermap.org/img/wn/${icon}@2x.png` 
+
+                            // fetch(weatherApiIcon)
+                            //     .then(function(response) {
+                            //         if (response.ok) {
+                            //             return response.blob();
+                            //         } else {
+                            //             alert('Error: ' + response.statusText);
+                            //         }
+                            //     })
+                            //     .then(function(blob) {
+                            //         const imgUrl = URL.createObjectURL(blob);
+                            //         const img = document.createElement('img');
+                            //         img.src = imgUrl;
+                            //         document.body.appendChild(img);
+                            //     })
+                            //     .catch(function(error) {
+                            //         console.log(error);
+                            //         alert(error.message);
+                            //     })
+                        }
+                    }
+                })
+                .catch(function(error) {
+                    console.log(error);
+                    alert(error.message);
+                });
+                })
         .catch(function(error) {
             console.log(error);
             alert(error.message);
         });
+        
 });
